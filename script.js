df00a40c1d97c1e8bce3706bdbc1d2bc8ea88af9
NeonEvergreen <nikhilmenon1997@gmail.com>
2023-08-07 02:09:13 +0530

    Add player choice buttons

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
 "use strict";
 
-const start_button = document.querySelector(".start.button");
+
+// element identifications
+
+const button = document.querySelectorAll(".start-button");
+const player = document.getElementById("player");
+const computer = document.getElementById("computer");
 const result = document.getElementById("result");
 
+// Initialising inner Texts
+player.innerText = null;
+computer.innerText = null;
 
+//Hover functionality for buttons
 function hover(element, enter_func, leave_func){
     element.addEventListener('mouseenter', enter_func);
     element.addEventListener('mouseleave', leave_func);
 }
 
-
-hover(start_button,() => { start_button.classList.add("button_hovered") },
-() => {  start_button.classList.remove("button_hovered") })
-
-
-const options = ["rock", "paper", "scissor"]
+button.forEach((choice_button) => hover(choice_button, () => { choice_button.classList.add("button_hovered") },
+() => {  choice_button.classList.remove("button_hovered") }));
 
 
+//Game logic
+
+const options = ["rock", "paper", "scissor"];
+const status_ = [ "Its a Tie", "You have won!", "Computer has won!"];
+
+
+//// computer choice
 function play_rps(){
     return options[Math.floor(Math.random() * 3)];
 }
 
-const status_ = [ "Its a Tie", "You have won!", "Computer has won!"];
-
+//// Identifying winner
 function identify_winner(choice_1, choice_2){
     
     if(choice_1 == "rock" && choice_2 == "rock"){
@@ -63,22 +74,44 @@ function identify_winner(choice_1, choice_2){
 
 }
 
-function play_match(){
- let computer_choice = play_rps();
- let player_choice = play_rps();
+//// Update status
 
- let winner = identify_winner(computer_choice, player_choice);
+function declareWinner(winner_number){
 
- let message = status_[winner];
+    let message = status_[winner_number];
 
- console.log(message);
+    console.log(message);
+   
+    result.innerText = message;
+   
+    winner_number == 1 ? result.style.backgroundColor = '#14d226' :  winner_number == 2 ? result.style.backgroundColor = '#FFCCCB' : null;   
 
- result.innerText = message;
-
- winner == 1 ? result.style.backgroundColor = '#14d226' :  winner == 2 ? result.style.backgroundColor = '#FFCCCB' : null;
- 
 }
 
+//// Initialise match
+function play_match(button_choice){
+
+    if (!button_choice) {
+        console.error("Invalid button_choice:", button_choice);
+        return;
+    }
+
+ let computer_choice = play_rps();
+
+let player_choice;
+
+if(button_choice.classList.contains("rock")) { player_choice = "rock";}
+if(button_choice.classList.contains("paper")) {  player_choice = "paper";}
+if(button_choice.classList.contains("scissor"))  { player_choice = "scissor";}
 
 
-start_button.addEventListener('click',play_match);
+ player.innerText = player_choice; 
+ computer.innerText = computer_choice;
+
+ let winner = identify_winner(player_choice, computer_choice);
+
+ declareWinner(winner);
+}
+
+//// Event listener for click
+button.forEach((button_choice) => button_choice.addEventListener('click', () => play_match(button_choice)));
